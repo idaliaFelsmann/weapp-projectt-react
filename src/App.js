034820d22eba28f42974b10d18f800c150a9dced
idaliaFelsmann <105213955+idaliaFelsmann@users.noreply.github.com>
2022-09-13 08:26:02 +0200

    footed link added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ export default function App() {
   return (
     <div className="App">
       Testing app 
+      <footer>
+        Coded by Idalia F. {" "}
+        <a href="https://github.com/idaliaFelsmann/weapp-projectt-react" target="_blank"> open-sourced on Github</a>
+      </footer>
     </div>
   );
 }
